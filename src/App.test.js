c998166a20ba605912e8e8a8a2bcedc344840789
Carson Cruz <carson@comcreate.org>
2025-03-02 22:21:20 -0800

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders MLB Trivia Challenge title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/MLB Trivia Challenge/i);
-  expect(titleElement).toBeInTheDocument();
-});
