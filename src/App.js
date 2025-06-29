0e6b16049e71ecfeb94c7912712a7eb0505ac3a7
Carson Cruz <carson@comcreate.org>
2025-06-28 22:24:43 -0700

    Create src/index.js entry point

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Ball Knowers</h1>
+        <p>Sports Trivia Challenge</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
